f3787b4e4ab6b243bc90b57ada4a4f5b3a09c8c3
sunahara15 <sunahara.15@gmail.com>
2023-03-21 06:19:57 +0900

    commit

---
+++ src/servlet/Login.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.LoginLogic;
+import model.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response)
+					throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	request.setCharacterEncoding("UTF-8");
+	String name = request.getParameter("name");
+	String pass = request.getParameter("pass");
+
+	User user = new User(name, pass);
+
+	LoginLogic loginLogic = new LoginLogic();
+	boolean isLogin = loginLogic.execute(user);
+
+
+	if(isLogin) {
+		HttpSession session = request.getSession();
+		session.setAttribute("loginUser", user);
+	}
+	RequestDispatcher dispatcher =
+		request.getRequestDispatcher(
+		"/WEB-INF/loginResult.jsp");
+	dispatcher.forward(request, response);
+
+	}
+}
